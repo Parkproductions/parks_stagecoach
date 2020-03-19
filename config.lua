87f679a92ec92c9e442d97f050ce3cd87eb33468
Parkproductions <declan.holt@fitcollege.edu.au>
2020-03-20 09:05:28 +1300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,20 @@
+Config               = {}
+
+Config.DrawDistance  = 10.0
+Config.MarkerColor   = {r = 204, g = 204, b = 0}
+Config.Blip          = {sprite = 311, color = 1}
+
+Config.Marker = {
+  ["saint_dennis"] = {name = 'Saint Dennis', x = 2683.95, y = -1441.86, z = 46.16},
+  ["rhodes"] = {name = 'Rhodes', x = -807.41, y = -1385.16, z = 43.49}
+}
+
+Config.StagecoachJobSprite = 1560611276 -- Stage Coach Job Blip sprite
+
+Config.SetJob = true -- Set job after Start working
+Config.JobName = "stage_coach_driver" -- Job name to set
+
+Config.Coords = {
+	vector3(-214.41, 646.37, 112.95),
+	vector3(-807.41, -1385.16, 43.49)
+}
