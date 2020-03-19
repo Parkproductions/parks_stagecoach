f33fced1cd3c3bfbd7494b6c8510a492e6cdc6e4
Declan Holt <declanholt@parkproductions.net>
2020-03-20 09:35:12 +1300

    blip testing

--- config.lua
+++ config.lua
@@ -5,8 +5,8 @@ Config.MarkerColor   = {r = 204, g = 204, b = 0}
 Config.Blip          = {sprite = 311, color = 1}
 
 Config.Marker = {
-  ["saint_dennis"] = {name = 'Saint Dennis', sprite = -118871741, x = 2683.95, y = -1441.86, z = 46.16},
-  ["rhodes"] = {name = 'Rhodes', sprite = -118871741, x = 1254.05, y = -1327.07, z = 76.89}
+  ["saint_dennis"] = {name = 'Saint Dennis', sprite = 0x3C5469D5, x = 2683.95, y = -1441.86, z = 46.16},
+  ["rhodes"] = {name = 'Rhodes', sprite = 0x3C5469D5, x = 1254.05, y = -1327.07, z = 76.89}
 }
 
 Config.StagecoachJobSprite = 1560611276 -- Stage Coach Job Blip sprite
