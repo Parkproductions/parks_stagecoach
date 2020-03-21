0a5041dd37eb55dca84c5ea501052f3dd28354d8
Declan Holt <declanholt@parkproductions.net>
2020-03-21 19:53:18 +1300

    added active checks in loop

--- server/main.lua
+++ server/main.lua
@@ -4,3 +4,8 @@ AddEventHandler("parks_stagecoach:NpcStart", function (pos)
     Fighters[pos] = _source
     TriggerClientEvent("parks_stagecoach:CreateNPC", -1, pos, true)
 end)
+
+RegisterServerEvent("parks_stagecoach:buy_small_stagecoach")
+AddEventHandler("parks_stagecoach:buy_small_stagecoach", function ()
+	 user.removeMoney(100)
+end)
