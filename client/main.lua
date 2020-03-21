cae0b8911da173240bc236451fc0b781df9e0218
Declan Holt <declanholt@parkproductions.net>
2020-03-21 16:56:11 +1300

    updated npc headings

--- client/main.lua
+++ client/main.lua
@@ -63,7 +63,7 @@ Citizen.CreateThread(function()
 
     for _, zone in pairs(Config.Marker) do
             print(menu_trigger_loc)
-            if GetDistanceBetweenCoords(zone.x, zone.y, zone.z,GetEntityCoords(PlayerPedId()),false)>2 and zone.name == menu_trigger_loc then
+            if (GetDistanceBetweenCoords(zone.x, zone.y, zone.z,GetEntityCoords(PlayerPedId()),false)>2) and (zone.name == menu_trigger_loc) then
                 Wait(200)
                 PromptDelete(StageCoachPrompt)
             end
