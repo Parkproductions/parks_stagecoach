fd890040fe6e870edb29ea05ef22967d1ca61c95
Declan Holt <declanholt@parkproductions.net>
2020-03-22 10:28:06 +1300

    added active checks in loop

--- server/main.lua
+++ server/main.lua
@@ -5,10 +5,10 @@ AddEventHandler("parks_stagecoach:NpcStart", function (pos)
     TriggerClientEvent("parks_stagecoach:CreateNPC", -1, pos, true)
 end)
 
-RegisterServerEvent("parks_stagecoach:buy_small_stagecoach")
-AddEventHandler("parks_stagecoach:buy_small_stagecoach", function ()
+RegisterServerEvent("parks_stagecoach:buy_stagecoach")
+AddEventHandler("parks_stagecoach:buy_stagecoach", function (stagecoach_cost)
 	local _source = source
 	TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
-	user.removeMoney(-100)
+	user.removeMoney(stagecoach_cost)
 	end)
 end)
