aa63387f54e5da7032bc41c86dde22232620cd9e
Declan Holt <declanholt@parkproductions.net>
2020-03-22 13:34:52 +1300

    added client event trigger

--- server/main.lua
+++ server/main.lua
@@ -10,6 +10,6 @@ AddEventHandler("parks_stagecoach:buy_stagecoach", function (stagecoach_cost)
 	local _source = source
 	TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
 	user.removeMoney(stagecoach_cost)
-	TriggerClientEvent("parks_stagecoach:SpawnWagon", user, stagecoach_cost)
+	TriggerClientEvent("parks_stagecoach:SpawnWagon", stagecoach_cost, true)
 	end)
 end)
