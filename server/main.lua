98fee6c83b7ed5a4d7cd0756dd60310c549fec1d
Declan Holt <declanholt@parkproductions.net>
2020-03-22 13:37:01 +1300

    added client event trigger

--- server/main.lua
+++ server/main.lua
@@ -1,15 +1,11 @@
 
-RegisterServerEvent("parks_stagecoach:NpcStart")
-AddEventHandler("parks_stagecoach:NpcStart", function (pos)
-    Fighters[pos] = _source
-    TriggerClientEvent("parks_stagecoach:CreateNPC", -1, pos, true)
-end)
 
 RegisterServerEvent("parks_stagecoach:buy_stagecoach")
 AddEventHandler("parks_stagecoach:buy_stagecoach", function (stagecoach_cost)
 	local _source = source
 	TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
 	user.removeMoney(stagecoach_cost)
-	TriggerClientEvent("parks_stagecoach:SpawnWagon", stagecoach_cost, true)
 	end)
+	TriggerClientEvent("parks_stagecoach:SpawnWagon", stagecoach_cost, true)
+	
 end)
