51c320c740c2894f770ae60676dbff9c51c77e97
Declan Holt <declanholt@parkproductions.net>
2020-03-23 21:03:07 +1300

    testing blips

--- client/main.lua
+++ client/main.lua
@@ -74,7 +74,7 @@ AddEventHandler("parks_stagecoach:StartCoachJob", function (zone, spawn_coach)
     while true do
     Wait(10)
         if GetDistanceBetweenCoords(Config.Destination.x, Config.Destination.y, Config.Destination.z,GetEntityCoords(PlayerPedId()),false)<5 then
-            local door = GetClosestVehicleDoor(spawn_coach)
+            
             TaskEnterVehicle(passenger_1_female, spawn_coach, 20000, 0, door, 2, 1, 0)
             print(spawn_coach)
             passenger_onboard = true
