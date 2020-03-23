5aff677d36c6afef1e5b254c1e2fcf519fe10c28
Declan Holt <declanholt@parkproductions.net>
2020-03-24 09:11:29 +1300

    set vehicle undriveable

--- client/main.lua
+++ client/main.lua
@@ -76,11 +76,12 @@ AddEventHandler("parks_stagecoach:StartCoachJob", function (zone, spawn_coach)
         
         if GetDistanceBetweenCoords(Config.Destination.x, Config.Destination.y, Config.Destination.z,GetEntityCoords(PlayerPedId()),false)<5 then
             
-            local spawn_coach = GetVehiclePedIsIn(PlayerPedId(),false)        
-            TaskEnterVehicle(passenger_1_female, spawn_coach, -1, 0, 2.0, 1, 0)
+            local spawn_coach = GetVehiclePedIsIn(PlayerPedId(),false) 
             local npc_group = GetPedRelationshipGroupHash(passenger_1_female)
             SetRelationshipBetweenGroups(0 , GetHashKey("PLAYER") , npc_group)
-            Wait(1000)
+            Wait(1000)       
+            TaskEnterVehicle(passenger_1_female, spawn_coach, -1, 0, 2.0, 1, 0)
+
             local passenger_onboard = true
             print(passenger_1_female)
 
