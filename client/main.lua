4fc5259de97b1e87d47acc9e3130794390b84db9
Declan Holt <declanholt@parkproductions.net>
2020-03-25 09:13:33 +1300

    added return to war menu test

--- client/main.lua
+++ client/main.lua
@@ -354,7 +354,7 @@ AddEventHandler("parks_stagecoach:SpawnBorrowedWagon", function (stagecoach_cost
     Wait(3000)
 
     EndStageCoachCam()
-    return zone_name
+    print(zone_name)
     TriggerEvent("parks_stagecoach:StartCoachJob", spawn_coach, zone_name)
 
 end)
