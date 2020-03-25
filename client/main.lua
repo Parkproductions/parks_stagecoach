c6b8150403c792515d07c6a1ad178560b47724fb
Declan Holt <declanholt@parkproductions.net>
2020-03-26 09:28:19 +1300

    blip sprite testing

--- client/main.lua
+++ client/main.lua
@@ -113,7 +113,7 @@ AddEventHandler("parks_stagecoach:StartCoachJob", function (zone_name, spawn_coa
     SetGpsMultiRouteRender(true)
 
     p1 = N_0x554d9d53f696d002(1664425300, Config.PickUp[zone_name][route].x, Config.PickUp[zone_name][route].y, Config.PickUp[zone_name][route].z)
-    SetBlipSprite(p1, Config.PickUp[zone_name][route].sprite, 5)
+    SetBlipSprite(p1, Config.Destination.sprite, 5)
     SetBlipScale(p1, 0.2)
     Citizen.InvokeNative(0x9CB1A1623062F402, p1, Config.PickUp[zone_name][route].sprite)
 
