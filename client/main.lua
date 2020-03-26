ec46dd445f2f12267cf28fc25e75a88c3d6eec9d
Declan Holt <declanholt@parkproductions.net>
2020-03-27 10:18:54 +1300

    stop driving function

--- client/main.lua
+++ client/main.lua
@@ -433,7 +433,7 @@ AddEventHandler("parks_stagecoach:DrivingStatus", function (driving)
     print(driving)
     local active = false
 
-    while driving do
+    while (driving == true)
         
         Wait(5)
         if IsControlJustPressed(0, keys['SPACE']) then 
