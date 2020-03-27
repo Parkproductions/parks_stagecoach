6884219c6caa75e85e7c257ad0fd16203a94d751
Declan Holt <declanholt@parkproductions.net>
2020-03-27 15:51:24 +1300

    added additional wagons

--- server/main.lua
+++ server/main.lua
@@ -4,15 +4,9 @@ RegisterServerEvent("parks_stagecoach:buy_stagecoach")
 AddEventHandler("parks_stagecoach:buy_stagecoach", function (stagecoach_cost)
 	local _source = source
 	TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
-	user.removeMoney(stagecoach_cost)
-		if stagecoach_cost == 0 then
-			TriggerClientEvent("parks_stagecoach:SpawnWagon", _source, stagecoach_cost)
-		else if stagecoach_cost == 100 then
-			TriggerClientEvent("parks_stagecoach:SpawnWagon", _source, stagecoach_cost)
-		end
-	end)
-	
-	
+	user.removeMoney(stagecoach_cost)	
+	TriggerClientEvent("parks_stagecoach:SpawnWagon", _source, stagecoach_cost)		
+	end)	
 end)
 
 
