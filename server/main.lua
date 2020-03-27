9e6ec3f6edae8ad84cf645aa8f1ae581108592af
Declan Holt <declanholt@parkproductions.net>
2020-03-28 12:22:04 +1300

    added sql insert when buying form menu

--- server/main.lua
+++ server/main.lua
@@ -47,7 +47,7 @@ AddEventHandler("parks_stagecoach:buy_stagecoach", function ( args )
     end)
 
     local _resul = GetAmmoutStagecoaches( u_identifier, u_charid )
-
+    print(_resul)
     if u_money <= _price then
         TriggerClientEvent( 'UI:DrawNotification', _src, Config.NoMoney )
         return
