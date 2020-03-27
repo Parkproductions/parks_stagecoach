c0378778579418a77e10ec9b4e6939cb19c52263
Declan Holt <declanholt@parkproductions.net>
2020-03-27 16:00:11 +1300

    added additional wagons

--- client/main.lua
+++ client/main.lua
@@ -320,16 +320,6 @@ AddEventHandler("parks_stagecoach:SpawnWagon", function (stagecoach_cost)
     
     if stagecoach_cost == 0 then
         local stage_coach = GetHashKey("WAGON02X")
-    elseif stagecoach_cost == 100 then
-        local stage_coach = GetHashKey("WAGON04X")
-    elseif stagecoach_cost == 500 then 
-        local stage_coach = GetHashKey("WAGON03X")
-    elseif stagecoach_cost == 850 then
-        local stage_coach = GetHashKey("WAGON05X")
-    elseif stagecoach_cost == 1000 then
-        local stage_coach = GetHashKey("WAGON06X")
-    else
-        local stage_coach = GetHashKey("BUGGY02")
     end
 
     print('cost', stagecoach_cost)
