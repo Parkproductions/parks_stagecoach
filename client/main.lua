d1731f169db3d46c717874fe654de5c61089a282
Declan Holt <declanholt@parkproductions.net>
2020-03-30 10:02:14 +1300

    changed driving status to function

--- client/main.lua
+++ client/main.lua
@@ -184,9 +184,9 @@ AddEventHandler("parks_stagecoach:StartCoachJob", function (zone_name, spawn_coa
     SetBlipSprite(p1, Config.PickUp[zone_name][route].sprite, 1)
     SetBlipScale(p1, 1)
     Citizen.InvokeNative(0x9CB1A1623062F402, p1, Config.PickUp[zone_name][route].name)
-
+    test = 123
     --[[TriggerEvent("parks_stagecoach:DrivingStatus", driving)--]]
-    DrivingStatus()
+    DrivingStatus(test)
     while (passenger_despawned == true) do
     Wait(10)
         
@@ -525,7 +525,7 @@ AddEventHandler("parks_stagecoach:DrivingStatus", function ()  --]]
     function DrivingStatus()
     
     local active = false
-    
+    print(test)
     
         while true do
         Wait(5)
