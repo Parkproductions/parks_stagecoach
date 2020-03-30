a91d441392b7bcd8845c9b33ca9c74e4b89a01de
Declan Holt <declanholt@parkproductions.net>
2020-03-30 17:20:14 +1300

    tidy up of client script

--- client/main.lua
+++ client/main.lua
@@ -492,7 +492,7 @@ Citizen.CreateThread(function()
         if WarMenu.IsMenuOpened('DrivingStatusFalse') then
             WarMenu.Display()
             if WarMenu.Button("Start Driving") then
-                    TriggerEvent("parks_stagecoach:StartCoachJob", zone_name, spawn_coach, driving)
+                    TriggerEvent("parks_stagecoach:StartCoachJob", zone_name, spawn_coach, true)
                     WarMenu.CloseMenu()
                     Wait(600)
                     WarMenu.Display()
