ec7236f9b1645485fc9a2258e7492729ba84002e
Declan Holt <declanholt@parkproductions.net>
2020-04-02 14:07:09 +1300

    menu tidy up

--- client/main.lua
+++ client/main.lua
@@ -537,7 +537,7 @@ end)
 
 -- Warmenu Main Menu 
 
-Citizen.CreateThread(function()
+Citizen.CreateThread(function(HasStagecoaches)
     WarMenu.CreateMenu('Stagecoach_MainMenu', 'Coach Menu')
         WarMenu.CreateSubMenu('Stagecoach', 'Stagecoach_MainMenu', 'Buy a Stage Coach')
         WarMenu.CreateSubMenu('ListStagecoaches', 'Stagecoach_MainMenu', 'Owned Stage Coaches')
