616bb05a0b3262c46a2b4e9629ae3cc5e2749d09
Parkproductions <declanholt@parkproductions.net>
2020-04-20 17:41:33 +1200

    added notification display test

--- client/main.lua
+++ client/main.lua
@@ -7,28 +7,25 @@ local keys = { ['O'] = 0xF1301666, ['G'] = 0x5415BE48 }
 
 -- Sidebar Notification
 
-function DisplayLeftNotification(title, subTitle, iconDict, icon, duration) {
-  const struct1 = new DataView(new ArrayBuffer(4 * 4));
-  struct1.setInt32(0, duration, true);
+function DisplayLeftNotification(title, subTitle, iconDict, icon, duration)
   
-  const string1 = CreateVarString(10, "LITERAL_STRING", title);
-  const string2 = CreateVarString(10, "LITERAL_STRING", subTitle);
+    const struct1 = new DataView(new ArrayBuffer(4 * 4));
+    struct1.setInt32(0, duration, true);
   
-  const struct2 = new DataView(new ArrayBuffer(48));
-  struct2.setBigInt64(8, BigInt(string1), true);
-  struct2.setBigInt64(16, BigInt(string2), true);
-  struct2.setBigInt64(32, BigInt(GetHashKey(iconDict)), true);
-  struct2.setBigInt64(40, BigInt(GetHashKey(icon)), true);
+    const string1 = CreateVarString(10, "LITERAL_STRING", title);
+    const string2 = CreateVarString(10, "LITERAL_STRING", subTitle);
+  
+    const struct2 = new DataView(new ArrayBuffer(48));
+    struct2.setBigInt64(8, BigInt(string1), true);
+    struct2.setBigInt64(16, BigInt(string2), true);
+    struct2.setBigInt64(32, BigInt(GetHashKey(iconDict)), true);
+    struct2.setBigInt64(40, BigInt(GetHashKey(icon)), true);
 
-  Citizen.invokeNative("0x26E87218390E6729", struct1, struct2, 1, 1);
-}
-DisplayLeftNotification(
-  'Parks Stagecoach', 
-  '1st Coach Driven', 
-  'HUD_TOASTS',
-  'toast_player_deadeye',
-  10000
-)
+    Citizen.invokeNative("0x26E87218390E6729", struct1, struct2, 1, 1);
+
+    DisplayLeftNotification('Parks Stagecoach', '1st Coach Driven', 'HUD_TOASTS', 'toast_player_deadeye', 10000)
+
+end
 
     
 -- Create Wagon Wheel Map Marker
