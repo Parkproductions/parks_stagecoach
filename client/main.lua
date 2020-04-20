9d3e084734f745c0d9df2fa7fa8c3c3755a6a10b
Parkproductions <declanholt@parkproductions.net>
2020-04-20 18:06:16 +1200

    added notification display test

--- client/main.lua
+++ client/main.lua
@@ -9,21 +9,27 @@ local keys = { ['O'] = 0xF1301666, ['G'] = 0x5415BE48 }
 
 function DisplayLeftNotification(title, subTitle, iconDict, icon, duration)
   
-    struct1 = new DataView(new ArrayBuffer(4 * 4));
-    struct1.setInt32(0, duration, true);
+    --[[struct1 = new DataView(new ArrayBuffer(4 * 4));
+    struct1.setInt32(0, duration, true);--]]
   
-    string1 = CreateVarString(10, "LITERAL_STRING", title);
-    string2 = CreateVarString(10, "LITERAL_STRING", subTitle);
-  
-    struct2 = new DataView(new ArrayBuffer(48));
+    struct1 = duration
+
+    local string1 = CreateVarString(10, "LITERAL_STRING", title, Citizen.ResultAsLong())
+    local string2 = string2 = CreateVarString(10, "LITERAL_STRING", subTitle, Citizen.ResultAsLong())
+
+    --[[string1 = CreateVarString(10, "LITERAL_STRING", title);
+    string2 = CreateVarString(10, "LITERAL_STRING", subTitle);--]]
+    struct2 = { GetHashKey(iconDict), GetHashKey(icon)}
+
+--[[    struct2 = new DataView(new ArrayBuffer(48));
     struct2.setBigInt64(8, BigInt(string1), true);
     struct2.setBigInt64(16, BigInt(string2), true);
     struct2.setBigInt64(32, BigInt(GetHashKey(iconDict)), true);
-    struct2.setBigInt64(40, BigInt(GetHashKey(icon)), true);
+    struct2.setBigInt64(40, BigInt(GetHashKey(icon)), true);--]]
 
     Citizen.invokeNative("0x26E87218390E6729", struct1, struct2, 1, 1);
 
-    DisplayLeftNotification('Parks Stagecoach', '1st Coach Driven', 'HUD_TOASTS', 'toast_player_deadeye', 10000)
+    
 
 end
 
@@ -233,7 +239,7 @@ RegisterNetEvent("parks_stagecoach:StartCoachJob")
 AddEventHandler("parks_stagecoach:StartCoachJob", function (zone_name, spawn_coach, driving)
     
     -- Set driving status and select route 
-    DisplayLeftNotification()
+    DisplayLeftNotification('Parks Stagecoach', '1st Coach Driven', 'HUD_TOASTS', 'toast_player_deadeye', 10000)
     TriggerEvent("drivingtrue")
     zone_name = GetCurentTownName()
     local passenger_despawned = true
