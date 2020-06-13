8e4001555c15392e85acb59702aabf494d604428
Parkproductions <declanholt@parkproductions.net>
2020-06-13 15:49:20 +1200

    added coach health

--- client/main.lua
+++ client/main.lua
@@ -222,7 +222,7 @@ AddEventHandler("parks_stagecoach:PassengerOnboard", function (zone_name, route,
 
         town_name = GetCurentTownName()
         district_hash = GetDistrictHash()
-        print('Spawn Coach' spawn_coach, 'Coach Health' coach_health)
+        print('Spawn Coach', spawn_coach, 'Coach Health', coach_health)
        
         --[[print('Town', town_name, 'District', district_hash)--]]
         --[[TriggerEvent('redem_roleplay:Tip', "$" .. fare_amount, 2000)--]]
