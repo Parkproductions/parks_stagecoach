5ba624ef3a4281169d5ec9e476ca6039cfb863c9
Parkproductions <declanholt@parkproductions.net>
2020-08-05 21:15:12 +1200

    added playerinvehicle and calculate fare function

--- server/main.lua
+++ server/main.lua
@@ -1,7 +1,7 @@
 
 RegisterServerEvent("parks_stagecoach:pay_fare")
 AddEventHandler("parks_stagecoach:pay_fare", function (driver, fare)
-	local _source = driver
+	local _source = source
 	TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
 		user.addMoney(fare)
 	end)
